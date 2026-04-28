acd9e8df356cefff31a9fff0b595ba741c2d5aef
EZM <nathanmp201@gmail.com>
2026-04-28 11:59:44 -0300

    Update Funções.h

--- Funções Matématicas/Funções.h
+++ Funções Matématicas/Funções.h
@@ -1,1 +1,11 @@
+#include <iostream>
+#include <math.h>
+class funcoes
+{
+    public:
+        int MDC(int, int);
+        int SomaDigitos(unsigned long long int);
+        int ConverteBinario(unsigned long long int, int);
+        bool ConvertePrimo(int, int);
+};
 
