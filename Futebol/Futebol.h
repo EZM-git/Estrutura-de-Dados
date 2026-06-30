d410d7583bb2418e95ab5ba398cd67660e601f01
Nathan <nathanmp201@gmail.com>
2026-06-30 12:27:29 -0300

    Update Futebol.h

--- Futebol/Futebol.h
+++ Futebol/Futebol.h
@@ -1,1 +1,10 @@
+#include <iostream>
 
+class Futebol
+{
+    public:
+        std::string nome;
+        int habilidade;
+        Futebol *elo;
+        Futebol** Inserir(Futebol**, std::string, int);
+};
